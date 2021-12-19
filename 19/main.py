b6be6533b1ab862202869df588d18fc67afadbed
Flexlolo <Flexlololo@gmail.com>
2021-12-20 02:09:32 +0700

    fix the problem in day 19

--- 19/main.py
+++ 19/main.py
@@ -61,33 +61,17 @@ def point_overlap_max(points1, points2, best_return):
 	best = 0
 	best_pos = (0, 0, 0)
 
-	# for p1, p2 in product(points1, points2):
-	for p1_index in range(len(points1) - best_return):
-		p1 = points1[p1_index]
+	vectors = defaultdict(int)
 
-		for p2 in points2:
-			pos = tuple(p1[i] - p2[i] for i in range(3))
-				
-			points1_ = tuple(p for p in points1 if p != p1)
-			points2_ = tuple(p for p in points2 if p != p2)
+	for p1, p2 in product(points1, points2):
+		v = tuple(p2[i] - p1[i] for i in range(3))
+		vectors[v] += 1
 
-			count = 1
+	for v in vectors:
+		if vectors[v] >= best_return:
+			return vectors[v], tuple(-v[i] for i in range(3))
 
-			for p1_, p2_ in product(points1_, points2_):
-				pos_ = tuple(p1_[i] - p2_[i] for i in range(3))
-
-				if pos_ == pos:
-					count += 1
-
-			if count > best:
-				best = count
-				best_pos = pos
-
-			if count > best_return:
-				return best, best_pos
-
-	return best, best_pos
-
+	return 0, (0,0,0)
 
 
 def map_beacons(position, points, mapping):
@@ -113,14 +97,8 @@ def map_beacons(position, points, mapping):
 
 	return stack
 
-
-
-
 mapping = {0: ((0,0,0), rotations[0])}
 stack = [0]
-
-# ALREADY COMPUTED FROM PART 1
-# mapping = {0: ((0, 0, 0), (0, 0, 0)), 6: ((0, -31, 1119), (90, 180, 270)), 9: ((-1321, 2, -46), (0, 270, 180)), 18: ((-49, -24, -1280), (0, 0, 180)), 31: ((1238, -178, -78), (0, 90, 90)), 1: ((37, -80, -2444), (90, 0, 180)), 36: ((65, 1168, -1208), (0, 90, 0)), 5: ((1130, 1142, -1272), (0, 180, 0)), 8: ((-86, 1149, -2358), (90, 180, 90)), 24: ((-1162, 1081, -1316), (0, 90, 270)), 30: ((40, 2228, -1279), (90, 180, 90)), 15: ((1184, 2246, -1175), (90, 0, 0)), 20: ((-1314, 2341, -1255), (90, 0, 270)), 26: ((-126, 2258, -2504), (0, 270, 180)), 12: ((27, 3540, -2514), (0, 90, 270)), 34: ((-1178, 2303, -2387), (0, 90, 90)), 37: ((1165, 2344, -2394), (0, 180, 180)), 3: ((1209, 1115, -2505), (0, 180, 180)), 27: ((1121, 2312, -3580), (0, 90, 180)), 2: ((1245, 2227, -4750), (0, 0, 90)), 10: ((2404, 2318, -3662), (0, 90, 0)), 4: ((3577, 2304, -3694), (90, 180, 180)), 16: ((3660, 3611, -3682), (0, 270, 0)), 33: ((3615, 2401, -4791), (90, 0, 90)), 7: ((1216, 3439, -4790), (90, 180, 0)), 21: ((1173, 1062, -4865), (90, 0, 90)), 32: ((1211, 2274, -5952), (0, 270, 0)), 11: ((1160, 1099, -6084), (0, 180, 90)), 35: ((-125, 2374, -5970), (0, 0, 180)), 38: ((1116, 3482, -5996), (0, 180, 0)), 25: ((69, 2375, -7236), (90, 180, 180)), 23: ((1261, -21, -2424), (0, 270, 270)), 19: ((2385, 2256, -1269), (0, 90, 90)), 29: ((1254, 2329, 40), (0, 90, 0)), 17: ((3541, 2416, -1333), (0, 90, 180)), 22: ((-1255, 1192, -127), (0, 180, 270)), 28: ((-2357, 1058, -1236), (90, 0, 270)), 13: ((-31, 1180, -3732), (0, 0, 270)), 14: ((-1186, 1025, -3561), (0, 270, 90))}
 
 print('STARTED MAPPING BEACONS')
 while stack:
@@ -132,7 +110,6 @@ while stack:
 
 	print('LOOKING FOR OVERLAPS OF', beacon)
 	stack += map_beacons(position, points, mapping)
-
 
 
 print('DONE MAPPING BEACONS')
